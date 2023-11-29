59ca5862c24a7d253e33d76890f938a3aab26c57
Oli Smith <smith.o@etoncollege.org.uk>
2023-11-29 17:12:45 +0000

    Smart Home Start

--- Smart Home/main.swift
+++ Smart Home/main.swift
@@ -7,5 +7,38 @@
 
 import Foundation
 
-print("Hello, World!")
+class SmartHome {
+    
+    private var devices = [Devices]()
+    
+    func getDevicesStatuses() {
+        
+    }
+    
+}
 
+class Devices: SmartHome {
+    private var isOn: Bool
+    private var intensity: Int
+    
+    
+    init (isOn: Bool, intensity: Int) {
+        self.isOn = isOn
+        self.intensity = intensity
+    }
+}
+
+class Lights: Devices {
+    let colour: String
+    let type: String
+    
+    init (isOn: Bool, intensity: Int, colour: String, type: String) {
+        self.colour = colour
+        self.type = type
+        super.init(isOn: isOn, intensity: intensity)
+    }
+}
+
+class Heaters: Devices {
+    
+}
